c9bc3370eb8ba675025eb68c441dc8f68cd46f08
Kak-Palel <farrelganen@gmail.com>
2024-05-09 21:09:40 +0700

    3d configuration

--- main.cpp
+++ main.cpp
@@ -1,13 +1,64 @@
 #include <iostream>
+#include <string>
 #include "raylib.h"
 #include "rcamera.h"
 
-//------------------------------------------------------------------------------------
-// Program main entry point
-//------------------------------------------------------------------------------------
-
 const int screenWidth = 2560;
 const int screenHeight = 1600;
+
+enum {MENU = 0, SETTING, GAMEPLAY, PAUSE, DEAD};
+
+class Kapal
+{
+    private:
+
+    protected:
+    Vector3 position;
+    virtual void draw() {DrawCube(position, 1.0f, 2.0f, 2.0f, RED);}
+    virtual void move() {}
+    
+    public:
+    Kapal(Vector3 pos) : position(pos) {}
+
+};
+
+class MKapal : Kapal
+{
+    private:
+    void draw() override
+    {
+        DrawCube(position, 2.0f, 2.0f, 1.0f, BROWN);
+    }
+
+    void move() override
+    {
+        if(IsKeyDown(KEY_W)) {position.x += 0.3;}
+        if(IsKeyDown(KEY_A)) {position.z -= 0.3;}
+        if(IsKeyDown(KEY_S)) {position.x -= 0.3;}
+        if(IsKeyDown(KEY_D)) {position.z += 0.3;}
+    }
+
+    public:
+    MKapal(Vector3 pos) : Kapal(pos)
+    {
+        camera.position = (Vector3){ pos.x-10.0f, pos.y+10.0f, pos.z};
+        camera.target = position;
+        camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
+        camera.fovy = 45.0f;                                // Camera field-of-view Y
+        camera.projection = CAMERA_PERSPECTIVE;             // Camera projection type
+    }
+
+    Camera camera;
+
+    void update()
+    {
+        move();
+        draw();
+        camera.position = { position.x - 10.0f, position.y + 10.0f, position.z };
+        camera.target = position;
+    }
+
+};
 
 int scrSize(int pixLen, char axis)
 {
@@ -21,69 +72,43 @@ int scrSize(int pixLen, char axis)
 
 int main(void)
 {
-    // Initialization
-    //--------------------------------------------------------------------------------------
+    InitWindow(screenWidth, screenHeight, "KAPAL");
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - 3d camera free");
+    MKapal main_kapal({0, 0, 0});
+    int gamestate = GAMEPLAY;
 
-    // Define the camera to look into our 3d world
-    Camera camera = { 0 };
-    camera.position = (Vector3){ 10.0f, 10.0f, 10.0f }; // Camera position
-    camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };      // Camera looking at point
-    camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };          // Camera up vector (rotation towards target)
-    camera.fovy = 45.0f;                                // Camera field-of-view Y
-    camera.projection = CAMERA_PERSPECTIVE;             // Camera projection type
-
-    Vector3 cubePosition = { 0.0f, 0.0f, 0.0f };
-    DisableCursor();                    // Limit cursor to relative movement inside the window
     ToggleFullscreen();
 
-    SetTargetFPS(60);                   // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
-
-    // Main game loop
-    while (!WindowShouldClose())        // Detect window close button or ESC key
+    SetTargetFPS(60);
+    
+    while (!WindowShouldClose())
     {
-        // Update
-        //----------------------------------------------------------------------------------
-        SetMousePosition(scrSize(GetScreenWidth()/2, 'x'), scrSize(GetScreenWidth()/2, 'y'));
-        camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };      // Camera looking at point
-        UpdateCamera(&camera, CAMERA_CUSTOM);
 
-        if (IsKeyPressed('Z')) camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };
-        //----------------------------------------------------------------------------------
-
-        // Draw
-        //----------------------------------------------------------------------------------
         BeginDrawing();
+        
+        switch (gamestate)
+        {
+        case MENU:
+            break;
+        case SETTING:
+            break;
+        case GAMEPLAY:
+            ClearBackground((Color){50, 80, 255});
 
-            ClearBackground(RAYWHITE);
+            BeginMode3D(main_kapal.camera);
 
-            BeginMode3D(camera);
+                main_kapal.update();
 
-                DrawCube(cubePosition, 2.0f, 2.0f, 2.0f, RED);
-                DrawCubeWires(cubePosition, 2.0f, 2.0f, 2.0f, MAROON);
-
-                DrawGrid(10, 1.0f);
+                DrawGrid(1000, 1);
 
             EndMode3D();
 
-            DrawRectangle( 10, 10, 320, 93, Fade(SKYBLUE, 0.5f));
-            DrawRectangleLines( 10, 10, 320, 93, BLUE);
-
-            DrawText("Free camera default controls:", 20, 20, 10, BLACK);
-            DrawText("- Mouse Wheel to Zoom in-out", 40, 40, 10, DARKGRAY);
-            DrawText("- Mouse Wheel Pressed to Pan", 40, 60, 10, DARKGRAY);
-            DrawText("- Z to zoom to (0, 0, 0)", 40, 80, 10, DARKGRAY);
+            break;
+        }
+        
 
         EndDrawing();
-        //----------------------------------------------------------------------------------
     }
-
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
-
+    CloseWindow();
     return 0;
 }
