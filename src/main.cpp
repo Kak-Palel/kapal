a3f8959685bcda2607264e20ad35b3b7ae72897d
Kak-Palel <farrelganen@gmail.com>
2024-05-11 21:02:15 +0700

    global camera

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,65 @@
 #include "raylib.h"
 #include "rcamera.h"
 #include "raymath.h"
+#include "rlgl.h"
 
 const int screenWidth = 2560;
 const int screenHeight = 1600;
 
 enum {MENU = 0, SETTING, GAMEPLAY, PAUSE, DEAD};
+
+class Sea
+{
+    private:
+    Mesh mesh;
+
+    public:
+    Sea()
+    {
+    }
+};
+
+class MyCam
+{
+    private:
+    Camera* cam;
+
+    public:
+    MyCam(Vector3 target)
+    {
+        cam  = new Camera();
+        cam->position = (Vector3){ target.x-10.0f, target.y+10.0f, target.z};
+        cam->target = target;
+        cam->up = (Vector3){ 0.0f, 1.0f, 0.0f };
+        cam->fovy = 45.0f;                                // Camera field-of-view Y
+        cam->projection = CAMERA_PERSPECTIVE;             // Camera projection type
+
+    }
+
+    float getdist(Vector3 point)
+    {
+        return Vector3Distance(point, cam->position);
+    }
+
+    void viewScope(Vector3* arr)
+    {
+        //TODO: get the 4 end point of the field of views
+        arr[0] = {0, 0, 0};
+        arr[1] = {0, 0, 0};
+        arr[2] = {0, 0, 0};
+        arr[3] = {0, 0, 0};
+    }
+
+    Camera* getCam()
+    {
+        return cam;
+    }
+
+    Vector3 getPos()
+    {
+        return cam->position;
+    }
+};
 
 class Kapal
 {
@@ -32,6 +86,8 @@ class MKapal : Kapal
     float throttle;
     float angle;
     float tempRoll;
+    float dist_cam2kapal;
+    MyCam* camera;
 
     void draw() override
     {
@@ -56,47 +112,55 @@ class MKapal : Kapal
 
         position.x += (baseSpeed + throttle)*sin(angle * DEG2RAD);
         position.z += (baseSpeed + throttle)*cos(angle * DEG2RAD);
+        CameraMoveForward(camera->getCam(), (baseSpeed + throttle)*sin(angle * DEG2RAD), true);
+        CameraMoveRight(camera->getCam(), (baseSpeed + throttle)*cos(angle * DEG2RAD), true);
 
         if(tempRoll < 0 && !IsKeyDown(KEY_A)) {tempRoll++;}
         else if(tempRoll > 0 && !IsKeyDown(KEY_D)) {tempRoll--;}
-
     }
 
     public:
-    MKapal(Vector3 pos, float initAngle) : Kapal(pos)
+    MKapal(Vector3 pos, float initAngle, MyCam* cam) : Kapal(pos)
     {
-        model = LoadModel("../assets/main_ship.obj");
+        camera = cam;
+        model = LoadModel("assets/main_ship.obj");
 
-        model.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = LoadTexture("../assets/main_ship/tex/ship_lambert3_Diffuse.png");
-        model.materials[0].maps[MATERIAL_MAP_ROUGHNESS].texture = LoadTexture("../assets/main_ship/tex/ship_lambert3_Glossiness.png");
-        model.materials[0].maps[MATERIAL_MAP_HEIGHT].texture = LoadTexture("../assets/main_ship/tex/ship_lambert3_Height.png");
-        model.materials[0].maps[MATERIAL_MAP_NORMAL].texture = LoadTexture("../assets/main_ship/tex/ship_lambert3_Normal.png");
+        model.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = LoadTexture("assets/main_ship/tex/ship_lambert3_Diffuse.png");
+        model.materials[0].maps[MATERIAL_MAP_ROUGHNESS].texture = LoadTexture("assets/main_ship/tex/ship_lambert3_Glossiness.png");
+        model.materials[0].maps[MATERIAL_MAP_HEIGHT].texture = LoadTexture("assets/main_ship/tex/ship_lambert3_Height.png");
+        model.materials[0].maps[MATERIAL_MAP_NORMAL].texture = LoadTexture("assets/main_ship/tex/ship_lambert3_Normal.png");
+        
         
 
         scale = 0.03f;
         angle = initAngle + 90;
 
-        camera.position = (Vector3){ pos.x-10.0f, pos.y+10.0f, pos.z};
-        camera.target = position;
-        camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
-        camera.fovy = 45.0f;                                // Camera field-of-view Y
-        camera.projection = CAMERA_PERSPECTIVE;             // Camera projection type
-
         throttle = 0;
         tempRoll = 0;
     }
 
-    Camera camera;
 
     void update()
     {
         move();
         draw();
-        camera.position = { position.x - 10.0f, position.y + 10.0f, position.z };
-        camera.target = position;
+        dist_cam2kapal = Vector3Distance(position, camera->getPos());
+        float zoomMove = GetMouseWheelMove();
+        
+        if((dist_cam2kapal > 2 && zoomMove > 0) || (dist_cam2kapal < 30 && zoomMove < 0))
+        {
+            CameraMoveForward(camera->getCam(), zoomMove, false);
+        }
     }
 
-    ~MKapal() {UnloadModel(model);}
+    ~MKapal()
+    {
+        UnloadTexture(model.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture);
+        UnloadTexture(model.materials[0].maps[MATERIAL_MAP_ROUGHNESS].texture);
+        UnloadTexture(model.materials[0].maps[MATERIAL_MAP_HEIGHT].texture);
+        UnloadTexture(model.materials[0].maps[MATERIAL_MAP_NORMAL].texture);
+        UnloadModel(model);
+    }
 };
 
 int scrSize(int pixLen, char axis)
@@ -113,7 +177,8 @@ int main(void)
 {
     InitWindow(screenWidth, screenHeight, "KAPAL");
 
-    MKapal main_kapal({0, 1.5, 0}, 0);
+    MyCam camera({0, 0, 0});
+    MKapal main_kapal({0, 1.5, 0}, 0, &camera);
     int gamestate = GAMEPLAY;
 
     ToggleFullscreen();
@@ -132,16 +197,15 @@ int main(void)
         case SETTING:
             break;
         case GAMEPLAY:
-            ClearBackground((Color){50, 80, 255});
+            ClearBackground((Color){WHITE});
 
-            BeginMode3D(main_kapal.camera);
+            BeginMode3D(*(camera.getCam()));
 
                 main_kapal.update();
-
+                
                 DrawGrid(1000, 1);
 
             EndMode3D();
-
             break;
         }
 
